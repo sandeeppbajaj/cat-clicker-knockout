2e2178f1c4225398c10988a6f8901067d42c5eb7
Sandeep Bajaj <sandeep.bajaj@ge.com>
2015-09-16 20:59:07 -0700

    Add Cat model and move all cat properties from viewmodel

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
-var ViewModel = function(){
+var Cat = function(){
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/cat1.jpg');
     this.nickNames = ko.observableArray(['Tabby','Bubbles','Kitty']);
-
-    this.incrementCounter = function(){
-        this.clickCount(this.clickCount() + 1);
-    };
-
     this.catLevel = ko.computed(function(){
         if(this.clickCount() > 10)
           return 'Infant';
         else
           return 'New Born';
     },this);
+
+};
+
+var ViewModel = function(){
+    this.currentCat = ko.observable(new Cat());
+
+    this.incrementCounter = function(){
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
+
 };
 
 ko.applyBindings(new ViewModel());
